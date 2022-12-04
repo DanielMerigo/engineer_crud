0ebffd32a3415c930078f0d234a8b515cfe9d28f
Daniel Merigo <merigodaniel@outlook.com>
2022-12-04 14:50:33 -0300

    add user crud

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 const express = require ('express');
 const app = express();
 const port = 5555;
-const path = require('path');
+const fs = require("fs");
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/home.html'))
+var users = []
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded( { extended: false }));
+
+
+app.get("/users-form", (req, res) => {
+    res.render('userForm', {
+        title: 'Users',
+        message: "Add a user",
+        btn1: "Submit"
+    });
 });
 
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname+'/login.html'))
+app.post("/users-form", (req, res) => {
+    req.body.id = uuidv4()
+    var value = req.body
+    users.push(value)
+    res.redirect("/users-list")
+    console.log("User as insert")
 });
 
-app.get("/projects", (req, res) => {
-    res.sendFile(path.join(__dirname+'/projects.html'))
+app.get('/users-list', (req, res) => {
+    res.render('index', {
+        title: 'Users',
+        message: "Lista de usuarios",
+        users:users,
+        btn1: "edit",
+        btn2: "delete"
+    });
 });
+
+//Tive que usar o get PQ o delete não estava puxando
+app.get("/users-delete/:id", (req,res) => {
+    let index = users.findIndex(i => i.id === req.params.id);
+    users.splice(index, 1)
+    console.log(users)
+    res.redirect("/users-list");
+});
+var idAtual = ""
+app.get("/users-edit/:id", (req, res) => {
+    idAtual = (req.params.id)
+    res.render('userEdit', {
+        title: 'Edit user',
+        users: users,
+        message: `Edit user id: ${req.params.id}`,
+        btn1: `Confirm`
+    });
+})
+
+app.post("/users-edit/:id", (req, res) => {
+    let index = users.findIndex(i => i.id === idAtual);
+    users[index].name = req.body.name
+    users[index].phone = req.body.phone
+    res.redirect("/users-list")
+})
 
 app.listen(port, () => {
-    console.log(`Server is online :) \nport: ${port}`)
+    console.log(`Server is online :) \nport: ${port}`);
 });
+
